06f93f4fabb2ab82895502ad3be5a1c4033b9e47
knt419 <knt419@gmail.com>
2014-07-08 07:00:17 +0900

    add files

---
+++ init.rb
@@ -0,0 +1,10 @@
+require 'overview_wikipage_hook_listener'
+
+Redmine::Plugin.register :overview_wikipage do
+  name 'Overview Wikipage plugin'
+  author 'Author name'
+  description 'This is a plugin for Redmine'
+  version '0.0.1'
+  url 'http://example.com/path/to/plugin'
+  author_url 'http://example.com/about'
+end
