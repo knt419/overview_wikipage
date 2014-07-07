6e7e83c17e998e48bc7a1fb2c3913d6774d308d6
knt419 <knt419@gmail.com>
2014-07-08 07:21:55 +0900

    update readme

--- init.rb
+++ init.rb
@@ -2,9 +2,9 @@ require 'overview_wikipage_hook_listener'
 
 Redmine::Plugin.register :overview_wikipage do
   name 'Overview Wikipage plugin'
-  author 'Author name'
+  author 'knt419'
   description 'This is a plugin for Redmine'
   version '0.0.1'
-  url 'http://example.com/path/to/plugin'
-  author_url 'http://example.com/about'
+  url 'https://github.com/knt419/overview_wikipage'
+  author_url 'https://github.com/knt419'
 end
