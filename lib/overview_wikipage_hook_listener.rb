e3f9cd8d6bb4a49b82824a854a36131241b04d9b
knt419 <knt419@gmail.com>
2014-07-08 19:22:35 +0900

    fix error when page is not found

--- lib/overview_wikipage_hook_listener.rb
+++ lib/overview_wikipage_hook_listener.rb
@@ -1,8 +1,10 @@
 ﻿class OverviewWikipageHookListener < Redmine::Hook::ViewListener
   def view_projects_show_left(context)
     html = ''
-	formatter = Redmine::WikiFormatting::Textile::Formatter.new(Wiki.find_page(context[:project].to_param, :project => context[:project]).text)
-	html << content_tag(:div, formatter.to_html().html_safe, :class => 'box')
+	if Wiki.find_page(context[:project].to_param, :project => context[:project]) != nil then
+	  formatter = Redmine::WikiFormatting::Textile::Formatter.new(Wiki.find_page(context[:project].to_param, :project => context[:project]).text)
+	  html << content_tag(:div, formatter.to_html().html_safe, :class => 'box')
+	end
 	html
   end
 end
