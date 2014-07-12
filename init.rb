6c33d4218f91790d0daf846fd0c45edf4e82a7f7
knt419 <knt419@gmail.com>
2014-07-12 22:19:05 +0900

    modify init.rb

--- init.rb
+++ init.rb
@@ -3,9 +3,9 @@ require 'overview_wikipage_hook_listener'
 
 Redmine::Plugin.register :overview_wikipage do
   name 'Overview Wikipage plugin'
-  author 'Author name'
+  author 'knt419'
   description 'This is a plugin for Redmine'
   version '0.0.2'
-  url 'http://github.com/knt419/overview_wikipage'
-  author_url 'http://github.com/knt419/'
+  url 'https://github.com/knt419/overview_wikipage'
+  author_url 'https://github.com/knt419'
 end
