9827932c15e8c3b025e31712c32db9ef5814c48b
knt419 <knt419@gmail.com>
2014-07-19 19:28:46 +0900

    now be able to specify the wikipage

--- init.rb
+++ init.rb
@@ -5,7 +5,11 @@ Redmine::Plugin.register :overview_wikipage do
   name 'Overview Wikipage plugin'
   author 'knt419'
   description 'This is a plugin for Redmine'
-  version '0.0.2'
+  version '0.0.3'
   url 'https://github.com/knt419/overview_wikipage'
   author_url 'https://github.com/knt419'
+
+  settings :default => {
+    'wikipage_name' => 'overview',
+  }, :partial => 'settings/overview_wikipage_settings'
 end
