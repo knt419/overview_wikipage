896e5e7af47105ed099826869a8525b84c00eff1
knt419 <knt419@gmail.com>
2017-12-01 21:56:20 +0900

    add .editorconfig

--- app/models/overview_page.rb
+++ app/models/overview_page.rb
@@ -2,4 +2,14 @@ class OverviewPage < ActiveRecord::Base
   unloadable
   belongs_to :project
   attr_accessible :pagename
+
+  def init(project_id)
+    overview_page = Project.find_by_id(project_id)
+
+    unless overview_page
+      overview_page = self.new
+      overview_page.project_id = project_id
+    end
+    overview_page
+  end
 end
