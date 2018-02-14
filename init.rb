5be23940bfbc49d4c2e3b3bfc59ecd556df2b84c
knt419 <knt419@gmail.com>
2018-02-14 23:15:56 +0900

    update plugin description

--- init.rb
+++ init.rb
@@ -5,7 +5,7 @@ require 'project_patch'
 Redmine::Plugin.register :overview_wikipage do
   name 'Overview Wikipage plugin'
   author 'knt419'
-  description 'This is a plugin for Redmine to show a wikipage in a project overview page'
+  description 'This is a Redmine plugin to show a wikipage in project overview'
   version '0.0.4'
   url 'https://github.com/knt419/overview_wikipage'
   author_url 'https://github.com/knt419'
